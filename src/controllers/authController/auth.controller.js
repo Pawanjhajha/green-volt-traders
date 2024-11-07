12c3a28edc07dbc36ef5a99204b506bd3e89b013
Pawan Kumar Jhajharia <pawankumarjhajharia3@gmail.com>
2024-11-07 20:09:50 +0530

    create the custome error class

--- src/controllers/authController/auth.controller.js
+++ src/controllers/authController/auth.controller.js
@@ -1,11 +1,19 @@
 
 import jwt from "jsonwebtoken";
+import { CutomeError } from "../../utils/customeError";
 export const login=async(req,res,next)=>{
 try{
     const{email,password}=req;
-    if(!email || !password){
-        throw new Error('')
+    if(!email){
+       const err= new CutomeError(400,'Bad Request',{
+        email:['The Email not provide']
+       });
     }
+    if(!password){
+      const err= new CutomeError(400,'Bad Request',{
+        password:['The Paaword not provide']
+      });
+   }
     const token=jwt.sign()
 }catch(error){
   next(error)//this will call the globle error handler and pass error object to err argument
--- src/controllers/globalErrorController.js
+++ src/controllers/globalErrorController.js
@@ -1,5 +1,5 @@
 //pass 4 argument to make the middilware as the global error handler .the first argument always be the error
-const globalErrorHandler=(err,req,res,next)=>{
+export const globalErrorHandler=(err,req,res,next)=>{
     //this error is going to recive the error Object.express app automatically recognize it as a global error handler.
     const statusCode = err.statusCode || 500;
     const message = err.message || 'Internal Server Error';
@@ -11,5 +11,3 @@ const globalErrorHandler=(err,req,res,next)=>{
       });
 
 }
-
-export default globalErrorHandler;
