7118124c611561e301f9e61e5f4fe7fad0345b5e
pawanjhajha <pawankumarjhajharia3@gmail.com>
2024-12-07 21:19:38 +0530

    hello

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/Button'
 
 function App() {
   return (
     <div className="App">
-    
+    <Button>click me</Button>
     </div>
   );
 }
